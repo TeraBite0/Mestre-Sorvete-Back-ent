13cd1b847174f50887fab728d3211a2eb43072b9
Kauan-deAlmeida <kauan.almeida@sptech.school>
2024-08-27 10:02:27 -0300

    Implementando service para estrutura spring boot

--- src/main/java/grupo/terabite/TeraBite/Controller/SorveteController.java
+++ src/main/java/grupo/terabite/TeraBite/Controller/SorveteController.java
@@ -2,7 +2,9 @@ package grupo.terabite.TeraBite.Controller;
 
 import grupo.terabite.TeraBite.Entity.Sorvete;
 import grupo.terabite.TeraBite.Repository.SorveteRepository;
+import grupo.terabite.TeraBite.Service.SorveteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,42 +15,31 @@ import java.util.Optional;
 public class SorveteController {
 
     @Autowired
-    private SorveteRepository sorveteRepository;
+    private SorveteService service;
 
     @GetMapping
     public List<Sorvete> listarTodos() {
-        return sorveteRepository.findAll();
+        return service.listarTodos();
     }
 
     @GetMapping("/{id}")
     public Sorvete buscarPorId(@PathVariable Integer id) {
-        Optional<Sorvete> sorvete = sorveteRepository.findById(id);
-        if(sorvete.isPresent()){
-            return sorvete.get();
-        }
-        return null;
+        return service.buscarPorId(id);
     }
 
     @PostMapping
     public Sorvete adicionarSorvete(@RequestBody Sorvete sorvete) {
-        Sorvete novoSorvete = sorveteRepository.save(sorvete);
-        return novoSorvete;
+        return service.adicionarSorvete(sorvete);
     }
+
 
     @PutMapping("/{id}")
     public Sorvete atualizarSorvete(@PathVariable Integer id, @RequestBody Sorvete sorveteAtualizado) {
-        if(sorveteRepository.existsById(id)){
-            sorveteAtualizado.setId(id);
-            return sorveteRepository.save(sorveteAtualizado);
-        }
-        return null;
-//        throw new RuntimeException("Sorvete com ID " + id + " não encontrado.");
+        return service.atualizarSorvete(id, sorveteAtualizado);
     }
 
     @DeleteMapping("/{id}")
     public void deletarSorvete(@PathVariable Integer id) {
-        if (sorveteRepository.existsById(id)) {
-            sorveteRepository.deleteById(id);
-        }
+        service.deletarSorvete(id);
     }
 }
